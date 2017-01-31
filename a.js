d1fd08fde6530f293e00c697f11e9daedbcb4f59
stardark999 <stardark999@gmail.com>
2017-01-30 20:04:44 -0800

    Update a.js

--- a.js
+++ a.js
@@ -1,5 +1,7 @@
 // Test file
 var a = document.getElementById("e").contentWindow.Classroom.send, b = 0;
+console.log(a);
+console.log(b);
 setInterval(function() { 
     a({action:'join-room', 'room-id':'1155'});
     a({action:'public-message', 'room-id':'1155', 'message':b++});
